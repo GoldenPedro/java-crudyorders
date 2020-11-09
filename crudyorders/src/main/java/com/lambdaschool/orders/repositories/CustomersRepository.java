cc18b62b1383eaea7f14bdd044aff7330b8be2cd
GoldenPedro <pedropablo@casuso.us>
2020-11-09 02:13:31 -0600

    completed mvp

---
+++ crudyorders/src/main/java/com/lambdaschool/orders/repositories/CustomersRepository.java
@@ -0,0 +1,29 @@
+package com.lambdaschool.orders.repositories;
+
+import com.lambdaschool.orders.models.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+/**
+ * The CRUD Repository connecting Customer to rest of the application.
+ */
+public interface CustomersRepository
+    extends CrudRepository<Customer, Long>
+{
+    /**
+     * List of customers whose name contains a given substring. Case insensitive search.
+     *
+     * @param name The substring to search names for.
+     * @return List of customers whose name contains the given substring.
+     */
+    List<Customer> findByCustnameContainingIgnoringCase(String name);
+
+    /**
+     * Returns the first associated customer for the given agent. If no customers are associated with this agent, returns null.
+     *
+     * @param agentid The agent you are seeking a customer for
+     * @return the first customer record who is associated with this agent.
+     * if no customers are associated with this agent, returns null.
+     */
+    Customer findFirstByAgent_Agentcode(long agentid);}
